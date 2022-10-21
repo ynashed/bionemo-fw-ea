e3701b433c957ab7c2aa2ee032e5e5606a8e5e08
George Armstrong <georgea@nvidia.com>
2022-10-21 20:57:47 +0000

    NEW dataframe based dataset

--- bionemo/data/validation_dataset.py
+++ bionemo/data/validation_dataset.py
@@ -1,13 +1,44 @@
-from typing import Union, Callable, List
+from typing import Union, Callable, List, Optional, Dict
 import os
 
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
-from torch.nn.functional import pad
 from pytorch_lightning import LightningDataModule
 import pandas as pd
 
-from nemo.collections.common.tokenizers import TokenizerSpec
+
+class DataFrameTransformDataset(Dataset):
+    def __init__(self,
+                 data_file: Union[str, bytes, os.PathLike],
+                 functions: List[Callable[[pd.Series], Dict[str, torch.TensorType]]],
+                 read_csv_args: Optional[dict] = None,
+                ):
+        """Dataset that is fundamentally a dataset but allows transforms on rows
+
+        Args:
+            data_file (Union[str, bytes, os.PathLike]): csv file for pandas
+            functions (List[Callable[[pd.Series], Dict[str, torch.TensorType]]]):
+                Each entry transforms a dataframe row to a dict of tensor to use.
+                Union of all function results on an entry will be used.
+                These can be loosely coupled to the structure of `data_file`.
+        """
+
+        if read_csv_args is None:
+            read_csv_args = dict()
+        self.df = pd.read_csv(data_file, **read_csv_args)
+        self.length = self.df.shape[0]
+        self.functions = functions
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        df_entry = self.df.iloc[idx]
+        result = {}
+        for fn in self.functions:
+            result.update(fn(df_entry))
+        return result
+
 
 class SingleValuePredictionDataset(Dataset):
     def __init__(self, 
