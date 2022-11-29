cd5ff165c217b5d8601111ec9b6a513ccef9b6b1
George Armstrong <georgea@nvidia.com>
2022-11-29 23:12:11 +0000

    add dnabert lr scheduler

--- bionemo/__init__.py
+++ bionemo/__init__.py
@@ -12,3 +12,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from nemo.core.optim.lr_scheduler import register_scheduler
+from bionemo.utils.scheduler import (
+    LinearWarmupHoldDecayPolicy,
+    LinearWarmupHoldDecayParams,
+)
+
+register_scheduler(
+    'LinearWarmupHoldDecayPolicy',
+    LinearWarmupHoldDecayPolicy,
+    LinearWarmupHoldDecayParams,
+)
+
