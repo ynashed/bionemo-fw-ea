4ea602f59a0bdfb0a31e4148c7bc52ae0ebab831
Michelle Gill <mgill@nvidia.com>
2023-02-07 09:11:11 -0800

    Testing improvements

--- tests/test_esm1nv_inference.py
+++ tests/test_esm1nv_inference.py
@@ -16,66 +16,67 @@
 import logging
 from contextlib import contextmanager
 import os
+import pathlib 
 
 from hydra import compose, initialize
-from hydra.core.plugins import Plugins
-from hydra.core.config_search_path import ConfigSearchPath
-from hydra.plugins.search_path_plugin import SearchPathPlugin
 from bionemo.model.protein.esm1nv import ESM1nvInference
+from bionemo.utils.tests import BioNemoSearchPathConfig, register_searchpath_config_plugin, update_relative_config_dir
 
 log = logging.getLogger(__name__)
 
-_INFERER = None
 CONFIG_PATH = "../examples/protein/esm1nv/conf"
 PREPEND_CONFIG_DIR = os.path.abspath("../examples/conf")
+MODEL_CLASS = ESM1nvInference
+
+####
+
+_INFERER = None
+os.environ["PROJECT_MOUNT"] = os.environ.get("PROJECT_MOUNT", '/workspace/bionemo')
+THIS_FILE_DIR = pathlib.Path(os.path.abspath(__file__)).parent
+
+def get_cfg(prepend_config_path, config_name, config_path='conf'):
+    prepend_config_path = pathlib.Path(prepend_config_path)
+
+    class TestSearchPathConfig(BioNemoSearchPathConfig):
+        def __init__(self) -> None:
+            super().__init__()
+            self.prepend_config_dir = update_relative_config_dir(prepend_config_path, THIS_FILE_DIR)
+
+    register_searchpath_config_plugin(TestSearchPathConfig)
+    with initialize(config_path=config_path):
+        cfg = compose(config_name=config_name)
+
+    return cfg
 
 @contextmanager
 def load_model(inf_cfg):
     global _INFERER
     if _INFERER is None:
-        _INFERER = ESM1nvInference(inf_cfg)
+        _INFERER = MODEL_CLASS(inf_cfg)
     yield _INFERER
 
 
-# TODO Move to module for use elsewhere -- requires different solution for passing prepended directory
-class SearchPathPrepend(SearchPathPlugin):
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        # Add search_path to the end of the existing search path
-        search_path.prepend( 
-            provider="searchpath-plugin", path=f"file://{PREPEND_CONFIG_DIR}"
-        )
-
-
-def register_searchpath_prepend_plugin() -> None:
-    """Call this function before invoking @hydra.main"""
-    Plugins.instance().register(SearchPathPrepend)
-
-
 def test_seq_to_embedding():
-    register_searchpath_prepend_plugin()
-    with initialize(config_path=CONFIG_PATH):
-        cfg = compose(config_name="infer")
-        with load_model(cfg) as inferer:
-            seqs = ['MSLKRKNIALIPAAGIGVRFGADKPKQYVEIGSKTVLEHVLGIFERHEAVDLTVVVVSPEDTFADKVQTAFPQVRVWKNGGQTRAETVRNGVAKLLETGLAAETDNILVHDAARCCLPSEALARLIEQAGNAAEGGILAVPVADTLKRAESGQISATVDRSGLWQAQTPQLFQAGLLHRALAAENLGGITDEASAVEKLGVRPLLIQGDARNLKLTQPQDAYIVRLLLDAV',
-                    'MIQSQINRNIRLDLADAILLSKAKKDLSFAEIADGTGLAEAFVTAALLGQQALPADAARLVGAKLDLDEDSILLLQMIPLRGCIDDRIPTDPTMYRFYEMLQVYGTTLKALVHEKFGDGIISAINFKLDVKKVADPEGGERAVITLDGKYLPTKPF']
-            embedding, mask = inferer.seq_to_hiddens(seqs)
-            assert embedding is not None
-            assert embedding.shape[0] == len(seqs)
-            assert len(embedding.shape) == 3
+    cfg = get_cfg(PREPEND_CONFIG_DIR, config_name='infer', config_path=CONFIG_PATH)
+    with load_model(cfg) as inferer:
+        seqs = ['MSLKRKNIALIPAAGIGVRFGADKPKQYVEIGSKTVLEHVLGIFERHEAVDLTVVVVSPEDTFADKVQTAFPQVRVWKNGGQTRAETVRNGVAKLLETGLAAETDNILVHDAARCCLPSEALARLIEQAGNAAEGGILAVPVADTLKRAESGQISATVDRSGLWQAQTPQLFQAGLLHRALAAENLGGITDEASAVEKLGVRPLLIQGDARNLKLTQPQDAYIVRLLLDAV',
+                'MIQSQINRNIRLDLADAILLSKAKKDLSFAEIADGTGLAEAFVTAALLGQQALPADAARLVGAKLDLDEDSILLLQMIPLRGCIDDRIPTDPTMYRFYEMLQVYGTTLKALVHEKFGDGIISAINFKLDVKKVADPEGGERAVITLDGKYLPTKPF']
+        embedding, mask = inferer.seq_to_hiddens(seqs)
+        assert embedding is not None
+        assert embedding.shape[0] == len(seqs)
+        assert len(embedding.shape) == 3
 
 
 def test_long_seq_to_embedding():
-    register_searchpath_prepend_plugin()
     long_seq = 'MIQSQINRNIRLDLADAILLSKAKKDLSFAEIADGTGLAEAFVTAALLGQQALPADAARLVGAKLDLDEDSILLLQMIPLRGCIDDRIPTDPTMYRFYEMLQVYGTTLKALVHEKFGDGIISAINFKLDVKKVADPEGGERAVITLDGKYLPTKPF'
     long_seq = long_seq * 10
-    with initialize(config_path=CONFIG_PATH):
-        cfg = compose(config_name="infer")
 
-        with load_model(cfg) as inferer:
-            seqs = ['MSLKRKNIALIPAAGIGVRFGADKPKQYVEIGSKTVLEHVLGIFERHEAVDLTVVVVSPEDTFADKVQTAFPQVRVWKNGGQTRAETVRNGVAKLLETGLAAETDNILVHDAARCCLPSEALARLIEQAGNAAEGGILAVPVADTLKRAESGQISATVDRSGLWQAQTPQLFQAGLLHRALAAENLGGITDEASAVEKLGVRPLLIQGDARNLKLTQPQDAYIVRLLLDAV',
-                    long_seq]
-            try:
-                inferer.seq_to_hiddens(seqs)
-                assert False
-            except Exception:
-                pass
+    cfg = get_cfg(PREPEND_CONFIG_DIR, config_name='infer', config_path=CONFIG_PATH)
+    with load_model(cfg) as inferer:
+        seqs = ['MSLKRKNIALIPAAGIGVRFGADKPKQYVEIGSKTVLEHVLGIFERHEAVDLTVVVVSPEDTFADKVQTAFPQVRVWKNGGQTRAETVRNGVAKLLETGLAAETDNILVHDAARCCLPSEALARLIEQAGNAAEGGILAVPVADTLKRAESGQISATVDRSGLWQAQTPQLFQAGLLHRALAAENLGGITDEASAVEKLGVRPLLIQGDARNLKLTQPQDAYIVRLLLDAV',
+                long_seq]
+        try:
+            inferer.seq_to_hiddens(seqs)
+            assert False
+        except Exception:
+            pass
