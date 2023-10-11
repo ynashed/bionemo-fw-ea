6f73289b6746e6c41f5f4ef58d8d69fc7a1f01a8
Steven Kothen-Hill <skothenhill@nvidia.com>
2023-10-11 14:08:23 -0700

    updated test to include instructions on generating dummy data.

---
+++ tests/_make_fake_uf50uf90.py
@@ -0,0 +1,31 @@
+
+alphabet='ARNDCQEGHILKMFPSTWYV'
+
+cluster_mapping = dict()
+with open('Fake50.fasta', 'w') as fd:
+    for length in range(50, 1000, 50):
+        for base in alphabet:
+            fd.write(f">UniRef50_{base}_{length}\n")
+            fd.write( base * length)
+            fd.write("\n")
+            cluster_mapping[f"UniRef50_{base}_{length}"] = list()
+
+
+
+with open('Fake90.fasta', 'w') as fd:
+    num_clusters = 10
+    for length in range(50, 1000, 50):
+        for base in alphabet:
+            for i in range(num_clusters):
+                fd.write(f">UniRef90_{base}_{length}-{i}\n")
+                fd.write( base * (length + i))
+                fd.write("\n")
+                cluster_mapping[f"UniRef50_{base}_{length}"].append(f"UniRef90_{base}_{length}-{i}")
+            num_clusters += 1
+
+
+with open("mapping.tsv", 'w') as fd:
+    fd.write(f"dumbheader\n")
+    for key, values in cluster_mapping.items():
+        str_ = key + "\t" + ",".join(values)
+        fd.write(f"{str_}\n")
--- tests/_test_esm2_uf90clustermapping_dataset.py
+++ tests/_test_esm2_uf90clustermapping_dataset.py
@@ -12,10 +12,12 @@ def test_uf90_resampling_dataset_integration():
     uf90_datapath: /data/uniref2022_05/fake-uniref/Fake90.fasta 
     dataset_path: /data/uniref2022_05/fake_uf_map
 
-    you can generate Fake50.fasta, Fake90.fasta, and mapping.tsv by running
+    you can generate Fake50.fasta, Fake90.fasta, and mapping.tsv by running this script with a python interpreter instead of pytest:
+        python tests/_test_esm2_uf90clustermapping_dataset.py
+    
+
+    files are deposited into the current working directory, move them to /data/uniref2022_05 before preprocessing.
     '''
-    
-    
     from omegaconf import OmegaConf
     cfg = OmegaConf.load('conf/uf90-resampling.yaml')
     trainer = setup_trainer(cfg, callbacks=[])
@@ -43,3 +45,37 @@ def test_uf90_resampling_dataset_integration():
         for uf50, uf90 in zip(ds.uniref50_dataset, ds):
             # Compares the IDs, where we manually constructed them to be nearly-equal.
             assert uf50.split("UniRef50_")[1] in uf90['sequence_id'].split("UniRef90_")[1]
+
+def make_data():
+    alphabet='ARNDCQEGHILKMFPSTWYV'
+    cluster_mapping = dict()
+    with open('Fake50.fasta', 'w') as fd:
+        for length in range(50, 1000, 50):
+            for base in alphabet:
+                fd.write(f">UniRef50_{base}_{length}\n")
+                fd.write( base * length)
+                fd.write("\n")
+                cluster_mapping[f"UniRef50_{base}_{length}"] = list()
+
+
+
+    with open('Fake90.fasta', 'w') as fd:
+        num_clusters = 10
+        for length in range(50, 1000, 50):
+            for base in alphabet:
+                for i in range(num_clusters):
+                    fd.write(f">UniRef90_{base}_{length}-{i}\n")
+                    fd.write( base * (length + i))
+                    fd.write("\n")
+                    cluster_mapping[f"UniRef50_{base}_{length}"].append(f"UniRef90_{base}_{length}-{i}")
+                num_clusters += 1
+
+
+    with open("mapping.tsv", 'w') as fd:
+        fd.write(f"dumbheader\n")
+        for key, values in cluster_mapping.items():
+            str_ = key + "\t" + ",".join(values)
+            fd.write(f"{str_}\n")
+
+if __name__ == '__main__':
+    make_data()
