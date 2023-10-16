3dc96ebed48ae8647399f917863c4d07874e2a77
srabhi <srabhi@nvidia.com>
2023-10-16 15:35:41 +0000

    fix linting

--- examples/protein/esm2nv/nbs/infer.py
+++ examples/protein/esm2nv/nbs/infer.py
@@ -23,13 +23,18 @@ from esm1nv_pb2_grpc import GenerativeSamplerStub
 
 log = logging.getLogger(__name__)
 
-MAX_MESSAGE_LENGTH=100 * 1024 * 1024
+MAX_MESSAGE_LENGTH = 100 * 1024 * 1024
+
+
 class ESMInferenceWrapper:
     def __init__(self):
-        channel = grpc.insecure_channel('localhost:50051',  options=[
-            ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
-            ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
-            ])
+        channel = grpc.insecure_channel(
+            'localhost:50051',
+            options=[
+                ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+                ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+            ],
+        )
         self.stub = GenerativeSamplerStub(channel)
 
     def seq_to_hiddens(self, seqs):
