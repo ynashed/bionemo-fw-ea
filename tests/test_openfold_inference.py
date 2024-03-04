90d5f4357354487a300dfd20abad7585f74b51f8
Kieran Didi <kdidi@nvidia.com>
2024-03-04 11:48:49 -0800

    add openfold inference validation tests

--- tests/test_openfold_inference.py
+++ tests/test_openfold_inference.py
@@ -1,48 +1,146 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
-#
-# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
-# property and proprietary rights in and to this material, related
-# documentation and any modifications thereto. Any use, reproduction,
-# disclosure or distribution of this material and related documentation
-# without an express license agreement from NVIDIA CORPORATION or
-# its affiliates is strictly prohibited.
-
 import os
 import tempfile
+from pathlib import Path
+from typing import Iterator, Tuple
 
 import pytest
+import pytorch_lightning as plt
 import torch
+from hydra import compose, initialize
+from hydra.core.global_hydra import GlobalHydra
+from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
 from bionemo.data.protein.openfold.datahub import get_structured_paths
 from bionemo.data.protein.openfold.datasets import PredictDataset
+from bionemo.data.protein.openfold.features import create_mmcif_features
 from bionemo.data.protein.openfold.helpers import collate
+from bionemo.data.protein.openfold.mmcif import load_mmcif_file, parse_mmcif_string
 from bionemo.model.protein.openfold.openfold_model import AlphaFold
+from bionemo.model.protein.openfold.validation_metrics import compute_validation_metrics
 from bionemo.model.protein.openfold.writer import PredictionFeatureWriter
 from bionemo.model.utils import setup_trainer
-from bionemo.utils.hydra import load_model_config
-from bionemo.utils.tests import teardown_apex_megatron_cuda
 
 
-MSA_DIR = os.path.join(os.environ["BIONEMO_HOME"], 'examples/tests/test_data/openfold_data/inference/msas')
-MSAS = [
-    ['7YVT_B_mgnify_alignment.a3m', '7YVT_B_smallbfd_alignment.a3m', '7YVT_B_uniref90_alignment.a3m'],
+GRADIENT_CHECKPOINTING = False
+
+INFERENCE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '../examples/tests/test_data/openfold_data/inference'
+)
+
+PDB_DIR = os.path.join(INFERENCE_DIR, 'pdb')
+PDB_NAMES = ['first_sequence.pdb', 'second_sequence.pdb']
+PDB_PATHS = [Path(os.path.join(PDB_DIR, pdb)) for pdb in PDB_NAMES]
+
+CIF_NAMES = ['first_sequence.cif', 'second_sequence.cif']
+CIF_PATHS = [Path(os.path.join(PDB_DIR, cif)) for cif in CIF_NAMES]
+CIF_CHAIN_IDS = ["A", "B"]
+
+MSA_DIR = os.path.join(INFERENCE_DIR, 'msas')
+MSA_NAMES = [
     ['7ZHL_A_mgnify_alignment.a3m', '7ZHL_A_smallbfd_alignment.a3m', '7ZHL_A_uniref90_alignment.a3m'],
+    ['7YVT_B_mgnify_alignment.a3m', '7YVT_B_smallbfd_alignment.a3m', '7YVT_B_uniref90_alignment.a3m'],
 ]
-MSAS = [[os.path.join(MSA_DIR, msa) for msa in msas] for msas in MSAS]
+MSA_PATHS = [[os.path.join(MSA_DIR, msa) for msa in msas] for msas in MSA_NAMES]
+
+PT_DIR = os.path.join(INFERENCE_DIR, 'pt')
+PT_NAMES = ['first_sequence.pt', 'second_sequence.pt']
+PT_PATHS = [Path(os.path.join(PT_DIR, pt)) for pt in PT_NAMES]
 
 
-@pytest.fixture(scope="module")
-def config_path(bionemo_home) -> str:
-    path = bionemo_home / "examples" / "protein" / "openfold" / "conf"
-    return str(path)
+@pytest.fixture(scope='module')
+def infer_cfg() -> Iterator[DictConfig]:
+    """Setting up the general inference config object.
+
+    Yields:
+        Iterator[DictConfig]: Inference Config object containing path and name
+    """
+    this_file_dir = os.path.dirname(os.path.realpath(__file__))
+    config_path = "examples/protein/openfold/conf"
+    config_name = "infer"
+    absolute_config_path = os.path.join(os.getenv("BIONEMO_HOME"), config_path)
+    relative_config_path = os.path.relpath(absolute_config_path, this_file_dir)
+    with initialize(config_path=relative_config_path):
+        cfg = compose(config_name=config_name)
+    yield cfg
+    GlobalHydra.instance().clear()
 
 
-@pytest.fixture(scope="module")
-def infer_cfg(config_path) -> str:
-    infer_cfg = load_model_config(config_name="infer", config_path=config_path)
+@pytest.fixture(scope='function')
+def alphafold_cfg(request, infer_cfg) -> DictConfig:
+    """Append additional parameters to the inference config specific to the instantiated AF model.
+
+    Args:
+        request: Request object containing the parameters that should be updated.
+        infer_cfg (DictConfig): Initial inference config object.
+
+    Returns:
+        DictConfig: Updated AlphaFold config.
+    """
+    msa_a3m_filepaths, generate_templates_if_missing = request.param
+    infer_cfg.model.data.msa_a3m_filepaths = msa_a3m_filepaths
+    infer_cfg.model.data.generate_templates_if_missing = generate_templates_if_missing
     return infer_cfg
+
+
+@pytest.fixture(scope='function')
+def alphafold_model_trainer(alphafold_cfg: DictConfig) -> Tuple[AlphaFold, plt.Trainer]:
+    """Setting up the AF model and trainer.
+
+    Args:
+        alphafold_cfg (DictConfig): Config object for model and trainer setup.
+
+    Returns:
+        Tuple[AlphaFold, plt.Trainer]: AlphaFold model and trainer.
+    """
+    trainer = setup_trainer(alphafold_cfg, callbacks=[])
+    alphafold = AlphaFold.restore_from(
+        restore_path=alphafold_cfg.restore_from_path, override_config_path=alphafold_cfg, trainer=trainer
+    )
+    return alphafold, trainer
+
+
+@pytest.fixture(scope='function')
+def alphafold_model(infer_cfg: DictConfig) -> AlphaFold:
+    """Given a config object, setup an AlphaFoldTest object from a checkpoint.
+
+    Args:
+        infer_cfg (DictConfig): Config Object containing path to checkpoint.
+
+    Returns:
+        AlphaFoldTest: Loaded AlphaFoldTest checkpoint.
+    """
+    trainer = setup_trainer(infer_cfg, callbacks=[])
+    alphafold = AlphaFold.restore_from(
+        restore_path=infer_cfg.restore_from_path, override_config_path=infer_cfg, trainer=trainer
+    )
+    return alphafold
+
+
+def get_predict_dataset(cfg: DictConfig) -> PredictDataset:
+    """Setup of prediction dataset for test purposes; it contains all input features for the
+    AlphaFold model, but not the ground truth coordinates.
+
+    Args:
+        cfg (DictConfig): Config file to genereate this dataset.
+
+    Returns:
+        PredictDataset: dataset object containing AF features for input sequences.
+    """
+    dataset_paths = get_structured_paths(cfg.model.data)
+    dataset = PredictDataset(
+        sequences=cfg.sequences,
+        seq_names=cfg.seq_names,
+        pdb_mmcif_chains_filepath=dataset_paths.mmcif_chains,
+        pdb_mmcif_dicts_dirpath=dataset_paths.mmcif_dicts,
+        pdb_obsolete_filepath=dataset_paths.obsolete_filepath,
+        template_hhr_filepaths=cfg.model.data.template_hhr_filepaths,
+        msa_a3m_filepaths=cfg.model.data.msa_a3m_filepaths,
+        generate_templates_if_missing=cfg.model.data.generate_templates_if_missing,
+        pdb70_database_path=cfg.model.data.pdb70_database_path,
+        cfg=cfg.model,
+    )
+    return dataset
 
 
 @pytest.mark.parametrize(
@@ -51,20 +149,27 @@ def infer_cfg(config_path) -> str:
         ['single', 'msa', 'pair', 'sm_single'],
     ],
 )
-def test_prediction_pdb_writer(infer_cfg, outputs):
+def test_openfold_prediction_pdb_writer(infer_cfg: DictConfig, outputs: str):
+    """Test if OpenFold inference and output writing works with different
+        output options (single, msa, pair, sm_single).
+
+    Args:
+        infer_cfg (DictConfig): Inference config specifying model options.
+        outputs (str): setting determining the output format
+    """
     # setup input/output samples for writer
     sequence = infer_cfg.sequences[0]
     seq_name = infer_cfg.seq_names[0]
     N_res = len(sequence)
-    bs = infer_cfg.model.micro_batch_size
+    batch_size = infer_cfg.model.micro_batch_size
     N_seq = 508
 
     input_dict = {'seq_name': [seq_name]}
     output_dict = {
-        'single': torch.empty(bs, N_res, infer_cfg.model.evoformer_stack_config.c_s),
-        'msa': torch.empty(bs, N_seq, N_res, infer_cfg.model.evoformer_stack_config.c_m),
-        'pair': torch.empty(bs, N_res, N_res, infer_cfg.model.evoformer_stack_config.c_z),
-        'sm_single': torch.empty(bs, N_res, infer_cfg.model.structure_module_config.c_s),
+        'single': torch.empty(batch_size, N_res, infer_cfg.model.evoformer_stack_config.c_s),
+        'msa': torch.empty(batch_size, N_seq, N_res, infer_cfg.model.evoformer_stack_config.c_m),
+        'pair': torch.empty(batch_size, N_res, N_res, infer_cfg.model.evoformer_stack_config.c_z),
+        'sm_single': torch.empty(batch_size, N_res, infer_cfg.model.structure_module_config.c_s),
     }
 
     # call writer
@@ -79,56 +184,108 @@ def test_prediction_pdb_writer(infer_cfg, outputs):
         )
 
 
-def get_predict_dataset(infer_cfg):
-    ds_paths = get_structured_paths(infer_cfg.model.data)
-    ds = PredictDataset(
-        sequences=infer_cfg.sequences,
-        seq_names=infer_cfg.seq_names,
-        pdb_mmcif_chains_filepath=ds_paths.mmcif_chains,
-        pdb_mmcif_dicts_dirpath=ds_paths.mmcif_dicts,
-        pdb_obsolete_filepath=ds_paths.obsolete_filepath,
-        template_hhr_filepaths=infer_cfg.model.data.template_hhr_filepaths,
-        msa_a3m_filepaths=infer_cfg.model.data.msa_a3m_filepaths,
-        generate_templates_if_missing=infer_cfg.model.data.generate_templates_if_missing,
-        pdb70_database_path=infer_cfg.model.data.pdb70_database_path,
-        cfg=infer_cfg.model,
-    )
-    return ds
-
-
 @pytest.mark.needs_gpu
 @pytest.mark.parametrize(
-    "msa_a3m_filepaths,generate_templates_if_missing",
+    "alphafold_cfg",
     [
         (
             [
                 [],
             ]
-            * len(MSAS),
+            * len(MSA_PATHS),
             False,
         ),  # sequence-only inference
-        (MSAS, False),  # sequence-and-msa inference without template
-        (MSAS, True),  # inference with template but no template dataset given
+        (MSA_PATHS, False),  # sequence-and-msa inference without template
+        (MSA_PATHS, True),  # inference with template but no template dataset given
     ],
+    indirect=True,
 )
-def test_openfold_inference(config_path, msa_a3m_filepaths, generate_templates_if_missing):
-    # setup config
-    cfg = load_model_config(config_name="infer", config_path=config_path)
-    cfg.model.data.msa_a3m_filepaths = msa_a3m_filepaths
-    cfg.model.data.generate_templates_if_missing = generate_templates_if_missing
+def test_openfold_inference_no_output_check(
+    alphafold_cfg: DictConfig, alphafold_model_trainer: Tuple[AlphaFold, plt.Trainer]
+):
+    """Testing if inference itself is running, with no output check.
 
+    Args:
+        alphafold_cfg (DictConfig): Config object for the model and dataset setup.
+        alphafold_model_trainer (Tuple[AlphaFold, plt.Trainer]): Model and Trainer for inference.
+    """
     # setup for inference
-    if generate_templates_if_missing:
+    if alphafold_cfg.model.data.generate_templates_if_missing:
         with pytest.raises(ValueError):  # raise error if generate template without template database
-            ds = get_predict_dataset(cfg)
+            dataset = get_predict_dataset(alphafold_cfg)
     else:
-        trainer = setup_trainer(cfg, callbacks=[])
-        alphafold = AlphaFold(cfg=cfg.model, trainer=trainer)  # TODO reduce model size in unittest
-        ds = get_predict_dataset(cfg)
+        alphafold_model, trainer = alphafold_model_trainer
+        dataset = get_predict_dataset(alphafold_cfg)
         dl = DataLoader(
-            ds, batch_size=cfg.model.micro_batch_size, num_workers=cfg.model.data.num_workers, collate_fn=collate
+            dataset,
+            batch_size=alphafold_cfg.model.micro_batch_size,
+            num_workers=alphafold_cfg.model.data.num_workers,
+            collate_fn=collate,
         )
-
+        assert len(dataset) > 0
         # inference
-        trainer.predict(alphafold, dl, return_predictions=False)
-        teardown_apex_megatron_cuda()
+        trainer.predict(alphafold_model, dl, return_predictions=False)
+
+
+@pytest.mark.xfail(reason="Missing MLPerf fix that is due to be merged")
+@pytest.mark.needs_gpu
+@pytest.mark.parametrize(
+    "alphafold_cfg",
+    [(MSA_PATHS, False)],
+    indirect=True,
+)
+def test_openfold_inference_lddt_validation_metric_check(alphafold_cfg: DictConfig):
+    """Test that checks whether the structure predicted by OpenFold is similar to the ground truth structure.
+    For this, the predicted and ground truth coordinates are both represented in atom37 format and fed into
+    the `compute_validation_metrics` function that computes the metric `{"lddt_ca"}`.
+    [Atom37 format information](https://huggingface.co/spaces/simonduerr/ProteinMPNN/blame/e65166bd70446c6fddcc1581dbc6dac06e7f8dca/alphafold/alphafold/model/all_atom.py)
+
+    In theory we could compute the metrics `{"lddt_ca", "alignment_rmsd", "gdt_ts", "gdt_ha"} via the `compute_validation_metrics` function.
+    We do not do this since lddt_ca is the only SE(3)-invariant metrics; all the others rely on alignments.
+    The default `superimpose` function used for this in `compute_validation_metrics` is not very good and gives
+    therefore incorrect results. Tests of incorporating Kabsch alignment showed that the metric computation itself
+    works when a better alignment algorithm is employed, but was not worth the compute requirement here.
+
+    Args:
+        alphafold_cfg (DictConfig): Config Object to restore AlphaFold model from checkpoint and initialise dataset.
+    """
+    # load ground truth data from mmcif files
+    mmcif_strings = [load_mmcif_file(mmcif_path) for mmcif_path in CIF_PATHS]
+    mmcif_dicts = [parse_mmcif_string(mmcif_string) for mmcif_string in mmcif_strings]
+    mmcif_features = [
+        create_mmcif_features(mmcif_dict, chain_id) for mmcif_dict, chain_id in zip(mmcif_dicts, CIF_CHAIN_IDS)
+    ]
+    ground_truth_atom_37_coords_list = [
+        torch.from_numpy(mmcif_feature["all_atom_positions"]) for mmcif_feature in mmcif_features
+    ]
+    # reshape by adding first dimension to match with the batch dimension layout of the predicted coords
+    ground_truth_atom_37_coords_list = [
+        torch.unsqueeze(gt_coords, 0) for gt_coords in ground_truth_atom_37_coords_list
+    ]
+    # setup for inference
+    trainer = setup_trainer(alphafold_cfg)
+    alphafold_model = AlphaFold.restore_from(
+        restore_path=alphafold_cfg.restore_from_path, override_config_path=alphafold_cfg, trainer=trainer
+    )
+    # get prediction dataset from config file, containing only sequences and MSAs, no structures
+    dataset = get_predict_dataset(alphafold_cfg)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=alphafold_cfg.model.micro_batch_size,
+        num_workers=alphafold_cfg.model.data.num_workers,
+        collate_fn=collate,
+    )
+    # inference
+    predicted_structures = trainer.predict(alphafold_model, data_loader, return_predictions=True)
+    # get the predicted atom14 coordinates and atom mask telling which positions are valid
+    predicted_atom_37_coords_list = [sample["final_atom_positions"] for sample in predicted_structures]
+    all_atom_mask_list = [sample["final_atom_mask"] for sample in predicted_structures]
+    # compare ground truth to predicted coordinates and calculate lddt_ca metric
+    metrics = {"lddt_ca"}
+    for predicted_atom_37_coords, ground_truth_atom_37_coord, all_atom_mask in zip(
+        predicted_atom_37_coords_list, ground_truth_atom_37_coords_list, all_atom_mask_list
+    ):
+        validation_metrics = compute_validation_metrics(
+            predicted_atom_37_coords, ground_truth_atom_37_coord, all_atom_mask, metrics
+        )
+        assert validation_metrics["lddt_ca"] > 90
