NeMo122 softmax fix + Golden value tests

bionemo/model/protein/esm1nv/attention.py
@@ -1,3 +1,15 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NvidiaProprietary
+#
+# NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
+# property and proprietary rights in and to this material, related
+# documentation and any modifications thereto. Any use, reproduction,
+# disclosure or distribution of this material and related documentation
+# without an express license agreement from NVIDIA CORPORATION or
+# its affiliates is strictly prohibited.
+
+from typing import Callable, Optional, Union
+
 import torch
 from einops import rearrange
 from nemo.collections.nlp.modules.common.megatron.adapters.parallel_adapters import (
@@ -177,6 +189,49 @@ class ESMnvCoreAttention(CoreAttention):
             )
         return context_layer
 
+    def esm2_scale_mask_softmax(
+        self,
+        input: torch.Tensor,
+        mask: Optional[torch.Tensor] = None,
+        scale: Optional[Union[float, int]] = None,
+        mask_func: Optional[Callable] = None,
+    ) -> torch.Tensor:
+        """Scale Mask Softmax function.
+
+        Args:
+            input: Tensor of shape (Batch, NP, SK, SQ). The input may or may not have already
+                had a mask applied to it.
+            mask: If a mask is to be applied, it will go here.
+            scale: A scale factor that will be applied before the softmax.
+            mask_func: An optional function to apply to the mask. If None, it is assumed that
+                the input already had the mask applied to it.
+
+        Returns:
+            probs: Tensor of normalized probabilities after the softmax has been applied,
+                of shape (Batch, NP, SK, SQ).
+        """
+        if self.attn_mask_type.name != "padding":
+            raise ValueError(
+                f"self.attn_mask_type: {self.attn_mask_type} is not 'padding'. Only 'padding' type is supported currently."
+            )
+
+        original_dtype = input.dtype  # Store original dtype
+        if (original_dtype == torch.float16 or original_dtype == torch.bfloat16) and self.attention_softmax_in_fp32:
+            input = input.float()  # Convert to float32 for softmax
+
+        if scale is not None:
+            input = input * scale  # Apply scaling
+
+        if mask is not None and mask_func is not None:
+            input = mask_func(input, mask)  # Apply mask function if provided
+
+        probs = torch.nn.functional.softmax(input, dim=-1)  # Apply softmax
+
+        if self.attention_softmax_in_fp32 and original_dtype in (torch.float16, torch.bfloat16):
+            probs = probs.to(original_dtype)  # Convert back to original dtype if necessary
+
+        return probs
+
     def torch_attention(self, query_layer, key_layer, value_layer, attention_mask, attention_bias, inference_mode):
         sq, b, np, hn = query_layer.shape
         sk = key_layer.shape[0]
@@ -190,7 +245,7 @@ class ESMnvCoreAttention(CoreAttention):
             key_layer = rearrange(key_layer, 'sk b np hn -> (b np) hn sk')
             value_layer = rearrange(value_layer, 'sv b np hn -> (b np) sv hn')
 
-        matmul_input_buffer = torch.empty(
+        matmul_input_buffer = torch.zeros(
             query_layer.shape[0],
             query_layer.shape[1],
             key_layer.shape[2],
@@ -208,17 +263,15 @@ class ESMnvCoreAttention(CoreAttention):
 
         # change view to [b, np, sq, sk]
         attention_scores = matmul_result.view(b, np, sq, sk)
-        # BIONEMO CUSTOM ATTENTION MASKING
         # TODO(srabhi, georgea): refactor the custom torch_attention module using Megatron Core when NeMo 1.21 is available
         if self.use_esm_attention:
             # NOTE: the slicing here is to make the attention_mask the same shape as the extended
             #  attention mask in ESM2. The multiplication by -3.4028e+38 is similarly motivated
             #  by ESM2's maskikng approach, which forces softmax of attention scores for
             #  masked entries to be close to 0.
-            attention_probs = self.scale_mask_softmax(
-                attention_scores + attention_mask[:, :, 0:1, :] * -3.4028e38, None
+            attention_probs = self.esm2_scale_mask_softmax(
+                attention_scores + attention_mask[:, :, 0:1, :] * -3.4028e38
             )
-        # END BIONEMO
         else:
             if attention_bias is not None:
                 attention_scores += attention_bias

bionemo/utils/tests.py
@@ -14,7 +14,8 @@ import os
 import pathlib
 import shutil
 from collections import OrderedDict
-from contextlib import contextmanager
+from contextlib import AbstractContextManager, contextmanager
+from dataclasses import dataclass
 from typing import Dict, List, Optional, Union
 
 import apex
@@ -28,6 +29,7 @@ from megatron.core.parallel_state import destroy_model_parallel
 from nemo.utils import logging
 from omegaconf import OmegaConf, open_dict
 from omegaconf.dictconfig import DictConfig
+from pytorch_lightning import seed_everything
 
 
 class BioNemoSearchPathConfig(SearchPathPlugin):
@@ -236,7 +238,38 @@ def distributed_model_parallel_state():
     from bionemo.model.utils import initialize_distributed_parallel_state  # here to avoid circular import
 
     try:
+        teardown_apex_megatron_cuda()
         initialize_distributed_parallel_state()
         yield
     finally:
         teardown_apex_megatron_cuda()
+
+
+@dataclass
+class Deterministic(AbstractContextManager):
+    cudnn_deterministic: Optional[bool] = None
+    cudnn_benchmark: Optional[bool] = None
+    cudnn_deterministic: Optional[bool] = None
+    cuda_matmul_allow_tf32: Optional[bool] = None
+    cudnn_enabled: Optional[bool] = None
+
+    def __enter__(self):
+        self.cudnn_deterministic = torch.backends.cudnn.deterministic
+        self.cudnn_benchmark = torch.backends.cudnn.benchmark
+        self.cuda_matmul_allow_tf32 = torch.backends.cuda.matmul.allow_tf32
+        self.cudnn_enabled = torch.backends.cudnn.enabled
+        seed_everything(123)
+        torch.use_deterministic_algorithms(False)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cuda.matmul.allow_tf32 = False
+        torch.backends.cuda.allow_tf32 = False
+        torch.backends.cudnn.enabled = False
+
+    def __exit__(self, *exit_state):
+        torch.use_deterministic_algorithms(False)
+        torch.backends.cudnn.deterministic = self.cudnn_deterministic
+        torch.backends.cudnn.benchmark = self.cudnn_benchmark
+        torch.backends.cuda.matmul.allow_tf32 = self.cuda_matmul_allow_tf32
+        torch.backends.cuda.allow_tf32 = True
+        torch.backends.cudnn.enabled = self.cudnn_enabled