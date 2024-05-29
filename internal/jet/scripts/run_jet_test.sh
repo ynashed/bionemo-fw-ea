b67cd57c8b83db917af3ecb53ae7bbe6f81f8bc0
Dorota Toczydlowska <dorotat@nvidia.com>
2024-05-28 23:49:20 -0700

    add tests to jet configs

--- internal/jet/scripts/run_jet_test.sh
+++ internal/jet/scripts/run_jet_test.sh
@@ -1,4 +1,3 @@
-# For internal use only. Should not be run manually. It is employed in CI.
 status=0
 
 jet_pipeline_id=$(curl --silent --header "PRIVATE-TOKEN: ${RO_API_TOKEN}" "https://gitlab-master.nvidia.com/api/v4/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/bridges" | jq '.[0].downstream_pipeline.id')
@@ -9,30 +8,25 @@ python internal/jet/get_results_from_jet.py --pipeline_id $jet_pipeline_id  --sa
 # to workload id and job key and creates workload_ids and job_keys arrays.
 # Save the original IFS
 filename="jet_query_${jet_pipeline_id}.json"
-workload_ids=($(jq -r '.[].jet_ci_workload_id' "$filename"))
+test_status=($(jq -r '.[].jet_test_status' "$filename"))
 job_keys=($(jq -r '.[].job_key' "$filename"))
 
 # Check if workload_ids is empty or if workload_ids and job_keys have different lengths
-if [[ -z "${workload_ids[*]}" || ${#workload_ids[@]} -ne ${#job_keys[@]} ]]; then
-  echo "Error: workload_ids is empty or workload_ids and job_keys have different lengths."
+if [[ -z "${test_status[*]}" || ${#test_status[@]} -ne ${#job_keys[@]} ]]; then
+  echo "Error: no jet tests run or jet_test and job_keys have different lengths."
   exit 1
 fi
 
 echo "==================================================================================================================="
 echo "============================================ STARTING JET TEST ===================================================="
 echo "==================================================================================================================="
-for (( i=1; i<${#workload_ids[@]}; ++i)); do
-  jet_log_id=${workload_ids[$i]}
+for (( i=1; i<${#test_status[@]}; ++i)); do
+  jet_test_status=${test_status[$i]}
   job_key=${job_keys[$i]}
-  if [[ "${job_key}" == recipe/* ]]
-  then
-    echo "\nJOB_KEY: ${job_key}, WORKLOAD ID: ${jet_log_id}"
-    jet tests run static --check-status --config "exit_codes[0]=0" --workload-log-id "${jet_log_id}" --origin "bionemo" --user "${GITLAB_USER_LOGIN}" --output "test-${jet_log_id}.zip"
-    job_status=$?
-    status=$(($job_status > $status ? $job_status : $status))
-    jet logs upload "test-${jet_log_id}.zip"
-    echo "JOB TEST STATUS: ${job_status}, PIPELINE TEST STATUS: ${status}"
+  if [[ "${job_key}" == recipe/* && "${jet_test_status}" != "Success" ]]; then
+      echo -e "\nJOB_KEY: ${job_key}, TEST: ${jet_test_status}"
+      status+=1
   fi
 done
-
+echo "PIPELINE TEST STATUS: ${status}"
 exit $status
