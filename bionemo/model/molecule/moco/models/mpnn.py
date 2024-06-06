66d6811f761cd05092a6b7e1e8b96ad030994756
Danny <dreidenbach@nvidia.com>
2024-06-05 18:59:11 -0700

    working egnn

--- bionemo/model/molecule/moco/models/mpnn.py
+++ bionemo/model/molecule/moco/models/mpnn.py
@@ -2,7 +2,10 @@ import math
 
 import torch
 import torch.nn as nn
+from torch_geometric.nn import MessagePassing, knn_graph
 from torch_geometric.nn.norm import LayerNorm as BatchLayerNorm
+from torch_scatter import scatter
+from torch_sparse import SparseTensor
 
 
 NONLINEARITIES = {
@@ -144,51 +147,6 @@ class AdaLN(nn.Module):
         return (1 + scale[batch]) * self.layernorm(h, batch) + shift[batch]
 
 
-class EGNN(nn.Module):
-    def __init__(self, invariant_feature_dim, equivariant_feature_dim):
-        self.time_embedder = TimestepEmbedder(invariant_feature_dim)
-        self.adaLN = AdaLN(invariant_feature_dim, invariant_feature_dim)
-        self.message_input_size = 0
-        self.h_input_size = 0
-        self.phi_message = MLP(self.message_input_size, invariant_feature_dim, invariant_feature_dim)
-        self.message_gate = MLP(invariant_feature_dim, invariant_feature_dim, 1)
-        self.phi_x = MLP(invariant_feature_dim, invariant_feature_dim, equivariant_feature_dim)
-        self.phi_h = MLP(self.h_input_size, invariant_feature_dim, invariant_feature_dim)
-        self.self.node_agg_scale = 1  # /100
-        self.coord_change_maximum = 10
-        # TODO: Understand how to do all to all message passing use EqGatDiff as a reference https://github.com/tuanle618/eqgat-diff/blob/68aea80691a8ba82e00816c82875347cbda2c2e5/eqgat_diff/experiments/diffusion_discrete_addfeats.py#L43
-        # ! This is the underlying MPNN they use EQGATGlobalEdgeConvFinal
-
-    def coord2radial(self, coord, edge_index):
-        row, col = edge_index
-        coord_diff = coord[row] - coord[col]
-        radial = torch.bmm(coord_diff, coord_diff.transpose(-1, -2))
-        return radial, coord_diff
-
-    def coord2diff(self, x, edge_index, norm=False, norm_constant=1):
-        row, col = edge_index
-        coord_diff = x[row] - x[col]
-        radial = torch.sum((coord_diff) ** 2, 1).unsqueeze(1)
-        norm = torch.sqrt(radial + 1e-8)
-        if norm:
-            coord_diff = coord_diff / (norm + norm_constant)
-        return radial, coord_diff
-
-    def forward(self, h, x, edge_index, edge_attr, mask):
-        src, dst = edge_index
-        hi, hj = h[src], h[dst]
-        radial, coord_diff = self.coord2radial(x, edge_index)
-        radial = radial.reshape(radial.shape[0], -1)
-        mij = self.phi_message(torch.cat([hi, hj, radial, edge_attr], -1))
-        gij = self.message_gate(mij)
-        h_agg = unsorted_segment_sum(mij * gij, src, num_segments=x.size(0))
-        h = h + self.phi_h(torch.cat([x, h_agg, h], -1))
-        trans = coord_diff * self.phi_x(mij).unsqueeze(-1)
-        x_agg = unsorted_segment_mean(trans, src, num_segments=x.size(0))
-        x = x + x_agg.clamp(-self.coord_change_maximum, self.coord_change_maximum)
-        return h, x
-
-
 def unsorted_segment_mean(data, segment_ids, num_segments):
     '''
     :param data: [n_edge, *dimensions]
@@ -221,6 +179,200 @@ def unsorted_segment_sum(data, segment_ids, num_segments):
     result = data.new_full(result_shape, 0)  # Init empty result tensor.
     result.scatter_add_(0, segment_ids, data)
     return result
+
+
+def get_triplet(edge_index: torch.Tensor, num_nodes: int):
+    """
+    Compute triplets of nodes and corresponding edge indices in a graph.
+
+    Args:
+        edge_index (torch.Tensor): The edge index tensor representing
+        the connections between source and target nodes.
+        num_nodes (int): The total number of nodes in the graph.
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+            A tuple containing:
+            - input_edge_index (torch.Tensor): The input edge index tensor.
+            - idx_i (torch.Tensor): Node indices i in the triplets (k->j->i).
+            - idx_j (torch.Tensor): Node indices j in the triplets (k->j->i).
+            - idx_k (torch.Tensor): Node indices k in the triplets (k->j->i).
+            - idx_kj (torch.Tensor): Edge indices (k-j) in the triplets.
+            - idx_ji (torch.Tensor): Edge indices (j->i) in the triplets.
+    """
+    assert edge_index.size(0) == 2
+    input_edge_index = edge_index.clone()
+    source, target = edge_index  # j->i
+    # create identifiers for edges based on (source, target)
+    value = torch.arange(source.size(0), device=source.device)
+    # as row-index select the target (column) nodes --> transpose
+    # create neighbours from j
+    adj_t = SparseTensor(row=target, col=source, value=value, sparse_sizes=(num_nodes, num_nodes))
+    # get neighbours from j
+    adj_t_row = adj_t[source]
+    # returns the target nodes (k) that include the source (j)
+    # note there can be path i->j->k where k is i
+    num_triplets = adj_t_row.set_value(None).sum(dim=1).to(torch.long)
+    # print(num_triplets)
+    # Node indices (k->j->i) for triplets.
+    idx_i = target.repeat_interleave(num_triplets)
+    idx_j = source.repeat_interleave(num_triplets)
+    idx_k = adj_t_row.storage.col()  # get index for k
+    mask = idx_i != idx_k  # Remove i == k triplets.
+    idx_i, idx_j, idx_k = idx_i[mask], idx_j[mask], idx_k[mask]
+    # print(idx_i); print(idx_j); print(idx_k)
+    # Edge indices (k-j, j->i) for triplets.
+    idx_kj = adj_t_row.storage.value()[mask]
+    idx_ji = adj_t_row.storage.row()[mask]
+
+    return input_edge_index, idx_i, idx_j, idx_k, idx_kj, idx_ji
+
+
+class EGNN(MessagePassing):
+    def __init__(
+        self,
+        equivariant_node_feature_dim=3,
+        invariant_node_feat_dim=64,
+        invariant_edge_feat_dim=5,
+    ):
+        super().__init__(node_dim=0, aggr=None, flow="source_to_target")  #! This should be target to source
+        self.pre_edge = MLP(invariant_edge_feat_dim, invariant_edge_feat_dim, invariant_edge_feat_dim)
+        self.edge_lin = MLP(2 * invariant_edge_feat_dim + 3, invariant_edge_feat_dim, invariant_edge_feat_dim)
+        self.message_input_size = invariant_node_feat_dim + invariant_node_feat_dim + 1 + invariant_edge_feat_dim
+        self.phi_message = MLP(self.message_input_size, invariant_node_feat_dim, invariant_node_feat_dim)
+        self.message_gate = MLP(invariant_node_feat_dim, invariant_node_feat_dim, 1)
+        self.phi_x = MLP(invariant_node_feat_dim, invariant_node_feat_dim, equivariant_node_feature_dim)
+        self.h_input_size = 2 * invariant_node_feat_dim
+        self.phi_h = MLP(self.h_input_size, invariant_node_feat_dim, invariant_node_feat_dim)
+        self.coor_update_clamp_value = 10.0
+        # self.reset_parameters()
+        self.h_norm = BatchLayerNorm(invariant_node_feat_dim)
+        self.apply(self.init_)
+
+    # def reset_parameters(self):
+    def init_(self, module):
+        if type(module) in {nn.Linear}:
+            # seems to be needed to keep the network from exploding to NaN with greater depths
+            nn.init.xavier_normal_(module.weight)
+            nn.init.zeros_(module.bias)
+
+    def mix_edges(self, batch, X, E, k=4):
+        num_nodes = X.size(0)
+        A_full = torch.zeros(
+            size=(num_nodes, num_nodes, E.size(-1)),
+            device=E.device,
+            dtype=E.dtype,
+        )  # N x N x num classes = 5
+        A_full[E_idx[0], E_idx[1], :] = E
+
+        # create kNN graph
+        edge_index_knn = knn_graph(x=X, k=k, batch=batch, flow="source_to_target")  # 2 x KE
+        j, i = edge_index_knn
+        p_ij = X[j] - X[i]
+        p_ij_n = torch.nn.functional.normalize(p_ij, p=2, dim=-1)
+        d_ij = torch.pow(p_ij, 2).sum(-1, keepdim=True).sqrt()
+
+        edge_ij = A_full[j, i, :]
+
+        edge_index_knn, _, _, _, idx_kj, idx_ji = get_triplet(
+            edge_index_knn, num_nodes=num_nodes
+        )  # 2 x KE,  M, M, M, M, M
+        p_jk = -1.0 * p_ij_n[idx_kj]
+        p_ji = p_ij_n[idx_ji]
+        theta_ijk = torch.arccos(torch.sum(p_jk * p_ji, -1, keepdim=True).clamp_(-1.0 + 1e-7, 1.0 - 1e-7))  # M x 1
+        d_ji = d_ij[idx_ji]  # M x 1
+        d_jk = d_ij[idx_kj]  # M x 1
+        edge_0 = edge_ij[idx_ji]  # M x 5
+        edge_1 = edge_ij[idx_kj]  # M x 5
+        f_ijk = torch.cat([edge_0, edge_1, theta_ijk, d_ji, d_jk], dim=-1)  # M x 2*classes + 3 = 13
+        f_ijk = self.edge_lin(f_ijk)  # M x num classes
+
+        aggr_edges = scatter(
+            src=f_ijk,
+            index=idx_ji,
+            dim=0,
+            reduce="mean",
+            dim_size=edge_index_knn.size(-1),
+        )  # KE x num_classes = 5
+        A_aggr = torch.zeros_like(A_full)  # N x N x 5
+        A_aggr[edge_index_knn[0], edge_index_knn[1], :] = aggr_edges
+        A_out = A_full + A_aggr  # N x N x 5
+        E = A_out[E_idx[0], E_idx[1], :]  # E x 5
+        return E
+
+    def forward(self, batch, X, H, edge_index, edge_attr):
+        # import ipdb; ipdb.set_trace()
+        edge_attr = self.pre_edge(edge_attr)
+        edge_attr = self.mix_edges(batch, X, edge_attr, k=4)
+        source, target = edge_index
+        rel_coors = X[source] - X[target]
+        rel_dist = (rel_coors**2).sum(dim=-1, keepdim=True)
+        edge_attr_feat = torch.cat([edge_attr, rel_dist], dim=-1)
+        # m_i, m_ij = self.propagate(edge_index=edge_index, X=X, X_rel=rel_coors, H=H, edge_attr=edge_attr_feat, batch=batch)
+        m_ij = self.propagate(edge_index=edge_index, X=X, X_rel=rel_coors, H=H, edge_attr=edge_attr_feat, batch=batch)
+        coor_wij = self.phi_x(m_ij)  # E x 3
+        if self.coor_update_clamp_value:
+            coor_wij.clamp_(min=-self.coor_update_clamp_value, max=self.coor_update_clamp_value)
+        X_rel_norm = rel_coors / (rel_coors.norm(dim=-1, keepdim=True).clamp(min=1e-8))  # TODO: add 1 + dij
+        x_update = scatter(
+            X_rel_norm * coor_wij, index=target, dim=0, reduce='mean', dim_size=X.shape[0]
+        )  # sum over all the src since its src to target
+        X_out = X + x_update  # TODO: add cross product like FlowMol and DiffSBDD
+        # m_i, m_ij = self.aggregate(inputs = m_ij * self.message_gate(m_ij), index = i, dim_size = X.shape[0])
+        m_i = scatter(m_ij * self.message_gate(m_ij), index=target, dim=0, reduce='mean', dim_size=X.shape[0])
+        # import ipdb; ipdb.set_trace()
+        # H_out = H + self.phi_h(torch.cat([H, m_i], dim = -1)) #self.h_norm(H, batch)
+        H_out = self.h_norm(
+            H + self.phi_h(torch.cat([H, m_i], dim=-1)), batch
+        )  # self.h_norm(H, batch) #! the use of LN here prevents H blow up similar to FlowMol
+        #! is the fact that FABind uses attention so a sfotmax weight (0,1) to scale before doing the residual update prevent explosion?
+        #! We use target as the index since we are aaggregating over i [1, 0] and [2, 0] we want to argegat over the 0 so we use its edge index
+        return X_out, H_out, edge_attr
+
+    def message(self, H_i, H_j, edge_attr):
+        # edge_attr already has the norm of distances
+        mij = self.phi_message(torch.cat([H_i, H_j, edge_attr], dim=-1))  # E x D
+        return mij
+
+    def aggregate(
+        self,
+        inputs,
+        index,
+        dim_size=None,
+    ):
+        # import ipdb; ipdb.set_trace()
+        # mi = scatter(inputs, index=index, dim=0, reduce="add", dim_size=dim_size) # index here is target
+        # return mi, inputs
+        return inputs
+
+    #  def coord2radial(self, coord, edge_index):
+    #     row, col = edge_index
+    #     coord_diff = coord[row] - coord[col]
+    #     radial = torch.bmm(coord_diff, coord_diff.transpose(-1, -2))
+    #     return radial, coord_diff
+
+    # def coord2diff(self, x, edge_index, norm=False, norm_constant=1):
+    #     row, col = edge_index
+    #     coord_diff = x[row] - x[col]
+    #     radial = torch.sum((coord_diff) ** 2, 1).unsqueeze(1)
+    #     norm = torch.sqrt(radial + 1e-8)
+    #     if norm:
+    #         coord_diff = coord_diff / (norm + norm_constant)
+    #     return radial, coord_diff
+
+    # def forward(self, h, x, edge_index, edge_attr, mask):
+    #     src, dst = edge_index
+    #     hi, hj = h_i, h_j
+    #     radial, coord_diff = self.coord2radial(x, edge_index)
+    #     radial = radial.reshape(radial.shape[0], -1)
+    #     mij = self.phi_message(torch.cat([hi, hj, radial, edge_attr], -1))
+    #     gij = self.message_gate(mij)
+    #     h_agg = unsorted_segment_sum(mij * gij, src, num_segments=x.size(0))
+    #     h = h + self.phi_h(torch.cat([x, h_agg, h], -1))
+    #     trans = coord_diff * self.phi_x(mij).unsqueeze(-1)
+    #     x_agg = unsorted_segment_mean(trans, src, num_segments=x.size(0))
+    #     x = x + x_agg.clamp(-self.coord_change_maximum, self.coord_change_maximum)
+    #     return h, x
 
 
 if __name__ == "__main__":
@@ -411,60 +563,6 @@ if __name__ == "__main__":
         ligand_feats.unsqueeze(1).float()
     ).unsqueeze(0)
 
-    #! this is used if self.bond pred is true but its not
-    # D = (X[E_idx[1]] - X[E_idx[0]]).pow(2).sum(-1, keepdim=True).sqrt() # E x 1
-    # E_idx = torch.concat([H[E_idx[1]] + H[E_idx[0]], D], dim=-1) # E x 64 + 1
-    from torch_sparse import SparseTensor
-
-    def get_triplet(edge_index: torch.Tensor, num_nodes: int):
-        """
-        Compute triplets of nodes and corresponding edge indices in a graph.
-
-        Args:
-            edge_index (torch.Tensor): The edge index tensor representing
-            the connections between source and target nodes.
-            num_nodes (int): The total number of nodes in the graph.
-
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-                A tuple containing:
-                - input_edge_index (torch.Tensor): The input edge index tensor.
-                - idx_i (torch.Tensor): Node indices i in the triplets (k->j->i).
-                - idx_j (torch.Tensor): Node indices j in the triplets (k->j->i).
-                - idx_k (torch.Tensor): Node indices k in the triplets (k->j->i).
-                - idx_kj (torch.Tensor): Edge indices (k-j) in the triplets.
-                - idx_ji (torch.Tensor): Edge indices (j->i) in the triplets.
-        """
-        assert edge_index.size(0) == 2
-        input_edge_index = edge_index.clone()
-        source, target = edge_index  # j->i
-        # create identifiers for edges based on (source, target)
-        value = torch.arange(source.size(0), device=source.device)
-        # as row-index select the target (column) nodes --> transpose
-        # create neighbours from j
-        adj_t = SparseTensor(row=target, col=source, value=value, sparse_sizes=(num_nodes, num_nodes))
-        # get neighbours from j
-        adj_t_row = adj_t[source]
-        # returns the target nodes (k) that include the source (j)
-        # note there can be path i->j->k where k is i
-        num_triplets = adj_t_row.set_value(None).sum(dim=1).to(torch.long)
-        # print(num_triplets)
-        # Node indices (k->j->i) for triplets.
-        idx_i = target.repeat_interleave(num_triplets)
-        idx_j = source.repeat_interleave(num_triplets)
-        idx_k = adj_t_row.storage.col()  # get index for k
-        mask = idx_i != idx_k  # Remove i == k triplets.
-        idx_i, idx_j, idx_k = idx_i[mask], idx_j[mask], idx_k[mask]
-        # print(idx_i); print(idx_j); print(idx_k)
-        # Edge indices (k-j, j->i) for triplets.
-        idx_kj = adj_t_row.storage.value()[mask]
-        idx_ji = adj_t_row.storage.row()[mask]
-
-        return input_edge_index, idx_i, idx_j, idx_k, idx_kj, idx_ji
-
-    import ipdb
-
-    ipdb.set_trace()
     source, target = E_idx
     r = X[target] - X[source]  # E x 3
     a = X[target] * X[source]
@@ -473,29 +571,17 @@ if __name__ == "__main__":
     d = d.sqrt()  # E
     r_norm = torch.div(r, (1.0 + d.unsqueeze(-1)))  # E x 3
     E = A[source, target]  # E x 5
-    E = torch.cat((d.unsqueeze(1), a.unsqueeze(1), r_norm, E), dim=-1)  # E x 10
-    import ipdb
+    E_all = torch.cat((d.unsqueeze(1), a.unsqueeze(1), r_norm, E), dim=-1)  # E x 10
+    edge_embedder = nn.Linear(5, 5)
+    #! Need to hit E with MLP first
+    E = edge_embedder(E.float())
 
-    ipdb.set_trace()
-    # Edge Message Passing
-    from torch_geometric.nn import knn_graph
+    # import ipdb; ipdb.set_trace()
+    model = EGNN()
+    print(X.sum(), H.sum(), E.sum())
+    for i in range(10):
+        X, H, E = model(batch_ligand, X, H, E_idx, E)
+        print(X.sum(), H.sum(), E.sum())
 
-    num_nodes = X.size(0)
-
-    E_full = torch.zeros(
-        size=(num_nodes, num_nodes, E.size(-1)),
-        device=E.device,
-        dtype=E.dtype,
-    )
-    E_full[E_idx[0], E_idx[1], :] = E
-
-    # create kNN graph
-    edge_index_knn = knn_graph(x=X, k=4, batch=batch_ligand, flow="source_to_target")
-    j, i = edge_index_knn
-    p_ij = X[j] - X[i]
-    p_ij_n = torch.nn.functional.normalize(p_ij, p=2, dim=-1)
-    d_ij = torch.pow(p_ij, 2).sum(-1, keepdim=True).sqrt()
-
-    edge_ij = E_full[j, i, :]
-
-    edge_index_knn, idx_i, idx_j, idx_k, idx_kj, idx_ji = get_triplet(edge_index_knn, num_nodes=num_nodes)
+    # import ipdb; ipdb.set_trace()
+    print("Success")
