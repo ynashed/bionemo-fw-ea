a855c02b33a55e1a339f8aff99bc56497c82bbaf
Jonathan Mitchell <jomitchell@nvidia.com>
2024-06-11 19:44:14 -0700

    Docker buildx bionemo1

--- launch.sh
+++ launch.sh
@@ -389,9 +389,33 @@ build() {
     #       Known bionemo build-time secrets:
     #         - GITLAB_TOKEN
     #
+    version_ge() {
+        # Returns 0 (true) if $1 >= $2, 1 (false) otherwise
+        [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$2" ]
+    }
+
+    # Check Docker version
+    docker_version=$(docker --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+    required_docker_version="23.0.1"
+
+    if ! version_ge "$docker_version" "$required_docker_version"; then
+        echo "Error: Docker version $required_docker_version or higher is required. Current version: $docker_version"
+        exit 1
+    fi
+
+    # Check Buildx version
+    buildx_version=$(docker buildx version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+    required_buildx_version="0.10.2"
+
+    if ! version_ge "$buildx_version" "$required_buildx_version"; then
+        echo "Error: Docker Buildx version $required_buildx_version or higher is required. Current version: $buildx_version"
+        exit 1
+    fi
+
     local created_at="$(date --iso-8601=seconds -u)"
-    DOCKER_BUILD_CMD="docker build --network host \
+    DOCKER_BUILD_CMD="docker buildx build --network host \
         -t ${IMAGE_NAME}:${IMAGE_TAG} \
+        --cache-to type=inline \
         --secret id=GITLAB_TOKEN,env=GITLAB_TOKEN \
         --label com.nvidia.bionemo.short_git_sha=${BIONEMO_SHORT_GIT_HASH} \
         --label com.nvidia.bionemo.git_sha=${BIONEMO_GIT_HASH} \
