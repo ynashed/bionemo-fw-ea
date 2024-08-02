55e361673f5896b1207bc9a2614191f296615ea9
Peter St. John <pstjohn@nvidia.com>
2024-08-02 14:18:30 -0700

    add some tests around the huggingface tokenizer

--- sub-packages/bionemo-esm2/tests/bionemo/esm2/data/test_tokenizer.py
+++ sub-packages/bionemo-esm2/tests/bionemo/esm2/data/test_tokenizer.py
@@ -17,12 +17,16 @@
 # Example proteins taken from the https://github.com/facebookresearch/esm main README.
 import pytest
 import torch
+from transformers import AutoTokenizer
 
-from bionemo.esm2.data.tokenizer import tokenize
+
+@pytest.fixture
+def tokenizer():
+    return AutoTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D")
 
 
-def test_tokenize_protein1():
-    our_tokens = tokenize("MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG")
+def test_tokenize_protein1(tokenizer):
+    our_tokens = tokenizer("MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG")["input_ids"][1:-1]
 
     # fmt: off
     esm_tokens = torch.tensor(
@@ -32,11 +36,13 @@ def test_tokenize_protein1():
          17, 12,  7,  5, 11, 14, 10,  6, 19,  7,  4,  5,  6,  6])
     # fmt: on
 
-    torch.testing.assert_close(our_tokens, esm_tokens)
+    torch.testing.assert_close(torch.tensor(our_tokens), esm_tokens)
 
 
-def test_tokenize_protein2():
-    our_tokens = tokenize("KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE")
+def test_tokenize_protein2(tokenizer):
+    our_tokens = tokenizer("KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE")["input_ids"][
+        1:-1
+    ]
 
     # fmt: off
     esm_tokens = torch.tensor(
@@ -47,11 +53,13 @@ def test_tokenize_protein2():
          16,  9,  9])
     # fmt: on
 
-    torch.testing.assert_close(our_tokens, esm_tokens)
+    torch.testing.assert_close(torch.tensor(our_tokens), esm_tokens)
 
 
-def test_tokenize_protein2_with_mask():
-    our_tokens = tokenize("KALTARQQEVFDLIRD<mask>ISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE")
+def test_tokenize_protein2_with_mask(tokenizer):
+    our_tokens = tokenizer("KALTARQQEVFDLIRD<mask>ISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE")[
+        "input_ids"
+    ][1:-1]
 
     # fmt: off
     esm_tokens = torch.tensor(
@@ -62,28 +70,24 @@ def test_tokenize_protein2_with_mask():
          16,  9,  9])
     # fmt: on
 
-    torch.testing.assert_close(our_tokens, esm_tokens)
+    torch.testing.assert_close(torch.tensor(our_tokens), esm_tokens)
 
 
-def test_tokenize_protein3():
-    our_tokens = tokenize("K A <mask> I S Q")
+def test_tokenize_protein3(tokenizer):
+    our_tokens = tokenizer("K A <mask> I S Q")["input_ids"][1:-1]
     esm_tokens = torch.tensor([15, 5, 32, 12, 8, 16])
-    torch.testing.assert_close(our_tokens, esm_tokens)
+    torch.testing.assert_close(torch.tensor(our_tokens), esm_tokens)
 
 
-def test_tokenize_non_standard_tokens():
-    our_tokens = tokenize(" ".join(["<cls>", "<pad>", "<eos>", "<unk>", "<mask>"]))
+def test_tokenize_non_standard_tokens(tokenizer):
+    our_tokens = tokenizer(" ".join(["<cls>", "<pad>", "<eos>", "<unk>", "<mask>"]))["input_ids"][1:-1]
     esm_tokens = torch.tensor([0, 1, 2, 3, 32])
-    torch.testing.assert_close(our_tokens, esm_tokens)
+    torch.testing.assert_close(torch.tensor(our_tokens), esm_tokens)
 
 
-def test_tokenize_raises_with_invalid_token():
-    with pytest.raises(KeyError):
-        tokenize("KALTARQ<invalid>ISQTGMPPTRAEI")
-
-    with pytest.raises(KeyError):
-        tokenize("/")
+def test_tokenize_with_invalid_token(tokenizer):
+    assert tokenizer("<invalid>")["input_ids"][1:-1] == [3]
 
 
-def test_tokenize_with_empty_string():
-    torch.testing.assert_close(tokenize(""), torch.tensor([], dtype=torch.int64))
+def test_tokenize_with_empty_string(tokenizer):
+    assert tokenizer("")["input_ids"] == [0, 2]
