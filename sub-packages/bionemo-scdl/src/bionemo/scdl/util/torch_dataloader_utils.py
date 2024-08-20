258e554f8c7fe0584d22cd2862e60e08d5568e5c
Polina Binder <pbinder@nvidia.com>
2024-08-19 18:39:56 -0700

    updated testing

--- sub-packages/bionemo-scdl/src/bionemo/scdl/util/torch_dataloader_utils.py
+++ sub-packages/bionemo-scdl/src/bionemo/scdl/util/torch_dataloader_utils.py
@@ -13,12 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List
+from typing import List, Optional
 
 import torch
 
 
-def collate_sparse_matrix_batch(batch: List[torch.Tensor]) -> torch.Tensor:
+def collate_sparse_matrix_batch(batch: List[torch.Tensor], max_dimesion: Optional[int] = None) -> torch.Tensor:
     """Collate function to create a batch out of sparse tensors.
 
     This is necessary to collate sparse matrices of various lengths.
@@ -29,9 +29,11 @@ def collate_sparse_matrix_batch(batch: List[torch.Tensor]) -> torch.Tensor:
     """
 
     batch_rows = torch.tensor([0] + [sparse_representation.shape[1] for sparse_representation in batch])
-    batch_cols = torch.cat([sparse_representation[1] for sparse_representation in batch])
+    batch_cols = torch.cat([sparse_representation[1] for sparse_representation in batch]).to(torch.int32)
     batch_values = torch.cat([sparse_representation[0] for sparse_representation in batch])
-
-    max_pointer = int(batch_cols.max().item() + 1)
+    if len(batch_cols) == 0:
+        max_pointer = 0
+    else:
+        max_pointer = int(batch_cols.max().item() + 1)
     batch_sparse_tensor = torch.sparse_csr_tensor(batch_rows, batch_cols, batch_values, size=(len(batch), max_pointer))
     return batch_sparse_tensor
