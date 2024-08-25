34da2e0844370c2e5a725937a38865231f6a1a94
Danny <dreidenbach@nvidia.com>
2024-08-24 18:09:14 -0700

    retrying slow

--- examples/molecule/moco/scripts/tmp_runs/danny_train_mega_ord_clip_dotfn.sh
+++ examples/molecule/moco/scripts/tmp_runs/danny_train_mega_ord_clip_dotfn.sh
@@ -23,7 +23,7 @@ CONFIG_NAME="train_dit_slurm_mega_clip_dotfn"
 # Logging
 # =========================
 EXP_GROUP="danny-ord-testing"
-EXP_NAME="ord_megalodon_bs150_clip_dotfn"
+EXP_NAME="ord_megalodon_bs150_clip_dotfn_redo"
 WANDB_ENTITY="clara-discovery"
 WANDB_GROUP=${EXP_GROUP}
 WANDB_PROJECT="MoCo"
