a9f0acd253a99397e3c400efc4301efa93d3eec7
John St John <jstjohn@nvidia.com>
2024-08-26 20:23:26 +0000

    Update ruff check issues

--- sub-packages/bionemo-llm/src/bionemo/llm/model/config.py
+++ sub-packages/bionemo-llm/src/bionemo/llm/model/config.py
@@ -32,13 +32,33 @@ class MegatronBioNeMoTrainableModelConfig(MegatronBioNeMoModelConfig[Model], Bio
 
 
 class IOMixinProto(Protocol):
-    def set_hparam(self, attribute: str, value: Any, also_change_value: bool = True) -> None: ...
-    def get_hparam(self, attribute: str) -> Any: ...
+    """A Protocol for the get/set hparam functions of the IOMixin class from NeMo."""
+
+    def set_hparam(self, attribute: str, value: Any, also_change_value: bool = True) -> None:
+        """Set the value of an attribute in the config attached to the class by the IOMixin."""
+        ...
+
+    def get_hparam(self, attribute: str) -> Any:
+        """Get the value of an attribute in the config attached to the class by the IOMixin."""
+        ...
 
 
 def override_mutate_possibly_extra_mutated_fiddle(
     target_cfg: IOMixinProto, source_cfg: IOMixinProto, maybe_mutated_elements_to_clone: List[str]
 ) -> None:
+    """Override the values of the target config with the values of the source config for the given elements.
+
+    This will modify the tracked init hyper-parameter values, as well as modifying the associated attributes in
+        self incase they were modified later by post_init code.
+
+    Args:
+        target_cfg: The config to update.
+        source_cfg: The config to copy values from.
+        maybe_mutated_elements_to_clone: The list of elements to copy from the source config to the target config.
+
+    Returns:
+        None, the target config is updated in place.
+    """
     for f in maybe_mutated_elements_to_clone:
         # 1. Update the tracked config values
         target_cfg.set_hparam(f, source_cfg.get_hparam(f))
