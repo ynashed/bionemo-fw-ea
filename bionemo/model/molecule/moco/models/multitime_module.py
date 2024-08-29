4d76ff67c997f7bc0d034737e75469be12c92702
Danny <dreidenbach@nvidia.com>
2024-08-29 07:16:15 -0700

    remove sample every val step

--- bionemo/model/molecule/moco/models/multitime_module.py
+++ bionemo/model/molecule/moco/models/multitime_module.py
@@ -348,7 +348,7 @@ class MultiTimeGraph3DInterpolantModel(pl.LightningModule):
         time = self.sample_time(batch)
         out, batch, time = self(batch, time)
         loss, predictions = self.calculate_loss(batch, out, time, "val")
-        self.sample(100)
+        # self.sample(100)
         return loss
 
     def training_step(self, batch, batch_idx):
