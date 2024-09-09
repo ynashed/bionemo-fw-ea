b0ff7d1e48e36bf420416fe341c5de67c1c691cb
Dejun Lin <dejunl@nvidia.com>
2024-09-09 21:21:44 +0000

    Enhancement: add option to supply dataset object

--- sub-packages/bionemo-size-aware-batching/src/bionemo/size_aware_batching/sampler.py
+++ sub-packages/bionemo-size-aware-batching/src/bionemo/size_aware_batching/sampler.py
@@ -15,10 +15,13 @@
 
 import sys
 from collections.abc import Sequence
-from typing import Callable, Dict, Iterable, Iterator, List, Union
+from typing import Callable, Dict, Generator, Iterable, List, TypeVar, Union
 from warnings import warn
 
 from torch.utils.data import Sampler
+
+
+Data = TypeVar("Data")
 
 
 class SizeAwareBatchSampler(Sampler[List[int]]):
@@ -29,8 +32,8 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
     Args:
         sampler (Sampler or Iterable): Base sampler. Can be any iterable object
         max_total_size (int or float): Max total size of the resulting mini-batch
-          calculated as a sum of per-element idx_to_size, will be disobeyed if only one element in a batch
-        idx_to_size (Iterable or Callable): idx_to_size[i] should be a size of i'th element in the dataset (eg number of nodes in the graph)
+          calculated as a sum of per-element sizeof, will be disobeyed if only one element in a batch
+        sizeof (Iterable or Callable): sizeof[i] should be a size of i'th element in the dataset (eg number of nodes in the graph)
             if callable, will take a sample, and return the size
         batch_size_mean (int, optional): average number of samples in one batch. Default to be None.
     """
@@ -39,9 +42,10 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
         self,
         sampler: Union[Sampler[List[int]], Iterable[int]],
         max_total_size: Union[int, float],
-        idx_to_size: Union[Dict[int, Union[int, float]], Sequence[int], Callable[[int], Union[int, float]]],
+        sizeof: Union[Dict[int, Union[int, float]], Sequence[int], Callable[[Data], Union[int, float]]],
         batch_size_mean: int,
         do_caching: bool = False,
+        dataset: Sequence[Data] = None,
     ) -> None:
         if not isinstance(max_total_size, (int, float)) or max_total_size <= 0:
             raise ValueError(f"max_total_size should be a positive number, but got max_total_size={max_total_size}")
@@ -50,15 +54,15 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
         self._batch_size_mean = batch_size_mean
         self._num_batches = (len(sampler) + self._batch_size_mean - 1) // self._batch_size_mean
 
-        self._is_idx_to_size_callable = callable(idx_to_size)
-        self._is_idx_to_size_dict = isinstance(idx_to_size, dict)
-        self._is_idx_to_size_seq = isinstance(idx_to_size, Sequence)
+        self._is_sizeof_callable = callable(sizeof)
+        self._is_sizeof_dict = isinstance(sizeof, dict)
+        self._is_sizeof_seq = isinstance(sizeof, Sequence)
 
-        if not (self._is_idx_to_size_callable or self._is_idx_to_size_dict or self._is_idx_to_size_seq):
-            raise ValueError("idx_to_size can only be a callable, a dictionary or a sequence container")
+        if not (self._is_sizeof_callable or self._is_sizeof_dict or self._is_sizeof_seq):
+            raise ValueError("sizeof can only be a callable, a dictionary or a sequence container")
 
-        if do_caching and (self._is_idx_to_size_dict or self._is_idx_to_size_seq):
-            raise ValueError("Caching is only supported for callable idx_to_size")
+        if do_caching and (self._is_sizeof_dict or self._is_sizeof_seq):
+            raise ValueError("Caching is only supported for callable sizeof")
 
         self._do_caching = do_caching
 
@@ -67,32 +71,42 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
         else:
             self._sizes_cache = None
 
+        if self._is_sizeof_callable and dataset is None:
+            raise ValueError("dataset should be provided when using callable sizeof")
+
+        if not self._is_sizeof_callable and dataset is not None:
+            raise ValueError(
+                "When using a predefined dict or sequenct container sizeof, dataset should not be provided"
+            )
+
+        self._dataset = dataset
+
         is_debug = hasattr(sys, "gettrace") and sys.gettrace() is not None
 
-        if is_debug and self._is_idx_to_size_seq:
+        if is_debug and self._is_sizeof_seq:
             # O(n) expensive check
             # check the bounds for the sample indices
             idx_min = min(sampler)
             idx_max = max(sampler)
-            size_idx = len(idx_to_size)
+            size_idx = len(sizeof)
             if idx_min < 0 or idx_min >= size_idx or idx_max < 0 or idx_max >= size_idx:
                 raise ValueError(
                     f"The index range of sampler [{idx_min}, {idx_max}] exceeds the index bounds of the sequence container [{0}, {size_idx-1}]"
                 )
 
-        if is_debug and (self._is_idx_to_size_dict or self._is_idx_to_size_seq):
+        if is_debug and (self._is_sizeof_dict or self._is_sizeof_seq):
             # O(n) expensive check
-            if self._is_idx_to_size_dict:
-                max_size = max(idx_to_size.values())
-                min_size = min(idx_to_size.values())
+            if self._is_sizeof_dict:
+                max_size = max(sizeof.values())
+                min_size = min(sizeof.values())
             else:
-                max_size = max(idx_to_size)
-                min_size = min(idx_to_size)
+                max_size = max(sizeof)
+                min_size = min(sizeof)
 
             if max_size > max_total_size:
                 warn(
                     "Sizes of some elements in the dataset exceed max_total_size "
-                    f"{max_total_size}. Such elements will be skipped. max(idx_to_size) = {max_size}"
+                    f"{max_total_size}. Such elements will be skipped. max(sizeof) = {max_size}"
                 )
             if min_size > max_total_size:
                 raise RuntimeError(
@@ -103,34 +117,35 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
 
         self._sampler = sampler
         self._max_total_size = max_total_size
-        self._idx_to_size = idx_to_size
+        self._sizeof = sizeof
 
     def __len__(self):
         return self._num_batches
 
-    def __iter__(self) -> Iterator[List[int]]:
+    def __iter__(self) -> Generator[List[int], None, None]:
         batch_total_size = 0
         yield_num_batches = 0
         batch = []
-        counter = 1
+        n_exhaustion = 1
         num_samples = 0
         while yield_num_batches < self._num_batches:
             # TODO: triage the necessity of this check
-            if counter > 1:
+            if n_exhaustion > 1:
                 warn(
-                    f"Sampler is being used {counter} times in one epoch, try to increase batch_size_mean = {self._batch_size_mean}, "
+                    f"Sampler is being exhausted more than once ({n_exhaustion} times) in one epoch, "
+                    f"try increasing batch_size_mean = {self._batch_size_mean}, "
                     f"yield_num_batches = {yield_num_batches}, num_batches = {self._num_batches}"
                 )
             for idx in self._sampler:
                 if self._sizes_cache is not None and idx in self._sizes_cache:
                     new_size = self._sizes_cache[idx]
-                elif self._is_idx_to_size_callable:
-                    new_size = self._idx_to_size(idx)
+                elif self._is_sizeof_callable:
+                    new_size = self._sizeof(self._dataset[idx])
                     if self._sizes_cache is not None:
                         self._sizes_cache[idx] = new_size
                 else:
-                    # self._idx_to_size is dict or sequence
-                    new_size = self._idx_to_size[idx]
+                    # self._sizeof is dict or sequence
+                    new_size = self._sizeof[idx]
                 if new_size > self._max_total_size:
                     warn(
                         f"Size of element {idx} exceeds max_total_size"
@@ -140,8 +155,8 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
                     continue
                 if new_size + batch_total_size > self._max_total_size:
                     num_samples += len(batch)
-                    yield batch
                     yield_num_batches += 1
+                    yield batch
                     batch_total_size = 0
                     batch = []
                 batch.append(idx)
@@ -153,7 +168,7 @@ class SizeAwareBatchSampler(Sampler[List[int]]):
                             f"try to increase num_batches = {self._num_batches} or reduce batch_size = {self._batch_size_mean}"
                         )
                     break
-            counter += 1
+            n_exhaustion += 1
             if num_samples == 0:
                 raise RuntimeError(
                     f"The underlying sampler has been exhausted once but no samples were generated. This could be due to "
