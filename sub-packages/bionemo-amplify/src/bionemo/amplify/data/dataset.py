26b12a06112c53e106d24d6390a5fbb08c4379da
Youssef Nashed <ynashed@nvidia.com>
2024-10-22 10:37:28 -0700

    Dataset resampling with MultiEpochDatasetResampler

--- sub-packages/bionemo-amplify/src/bionemo/amplify/data/dataset.py
+++ sub-packages/bionemo-amplify/src/bionemo/amplify/data/dataset.py
@@ -23,7 +23,7 @@ from torch.utils.data import Dataset
 
 from datasets import load_dataset as hf_load_dataset
 
-from bionemo.core.data.multi_epoch_dataset import EpochIndex, MultiEpochDatasetResampler
+from bionemo.core.data.multi_epoch_dataset import EpochIndex
 from bionemo.amplify.data import tokenizer
 from bionemo.llm.data import masking
 from bionemo.llm.data.types import BertSample
@@ -87,20 +87,19 @@ class AMPLIFYMaskedResidueDataset(Dataset):
         """
         return self.total_samples
 
-    def __getitem__(self, idx: int) -> BertSample:
+    def __getitem__(self, index: EpochIndex) -> BertSample:
         """Deterministically masks and returns a protein sequence from the dataset.
         Args:
-            idx: The current index of the sample to retrieve.
+            index: The current epoch and the index of the cluster to sample.
 
         Returns:
             A (possibly-truncated), masked protein sequence with CLS and EOS tokens and associated mask fields.
         """
-        if idx not in range(len(self)):
-            raise IndexError(f"Index {idx} out of range [0, {len(self)}).")
+        if index.idx not in range(len(self)):
+            raise IndexError(f"Index {index.idx} out of range [0, {len(self)}).")
 
-        # Initialize a random number generator with a seed that is a combination of the dataset seed and the index.
-        rng = np.random.default_rng([self.seed, idx])
-        sequence = self.protein_dataset[idx]["sequence"]
+        rng = np.random.default_rng([self.seed, index.epoch, index.idx])
+        sequence = self.protein_dataset[index.idx]["sequence"]
 
         # We don't want special tokens before we pass the input to the masking function; we add these in the collate_fn.
         tokenized_sequence = self._tokenize(sequence)
