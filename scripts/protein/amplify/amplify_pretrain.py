32aa17388d85e8de01fd68bdd710d37fc6638295
Youssef Nashed <ynashed@nvidia.com>
2024-12-13 16:16:44 -0800

    lower initial loss_scaling

--- scripts/protein/amplify/amplify_pretrain.py
+++ scripts/protein/amplify/amplify_pretrain.py
@@ -225,6 +225,7 @@ def main(
                 adam_beta1=0.9,
                 adam_beta2=0.95,
                 clip_grad=1.0,
+                initial_loss_scale=2**16,
             ),
             lr_scheduler=nl.lr_scheduler.CosineAnnealingScheduler(
                 min_lr=0.1*lr,
@@ -280,7 +281,7 @@ parser.add_argument(
     type=str,
     choices=get_args(PrecisionTypes),
     required=False,
-    default="fp16-mixed",
+    default="bf16-mixed",
     help="Precision type to use for training.",
 )
 parser.add_argument(
