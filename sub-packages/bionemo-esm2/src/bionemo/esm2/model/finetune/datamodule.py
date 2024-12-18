4d44f5d92f80f937d0f603ff9203346227812b19
Holger Roth <6304754+holgerroth@users.noreply.github.com>
2024-12-18 14:02:28 -0500

    Fix csv dataset (#543)
    Fix typo in label key.
    
    ## Summary
    Check for loading labels should use "labels" instead of "label". 
    
    ## Details
    Fixed the typo
    
    ## Usage
    n/a
    
    ## Testing
    no new tests
    
    ---------
    
    Signed-off-by: Holger Roth <hroth@nvidia.com>

--- sub-packages/bionemo-esm2/src/bionemo/esm2/model/finetune/datamodule.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/model/finetune/datamodule.py
@@ -79,7 +79,7 @@ class InMemoryCSVDataset(Dataset):
         sequence = self.sequences[index]
         tokenized_sequence = self._tokenize(sequence)
 
-        label = tokenized_sequence if len(self.labels) == 0 else self.labels[index]
+        label = tokenized_sequence if len(self.labels) == 0 else torch.Tensor([self.labels[index]])
         # Overall mask for a token being masked in some capacity - either mask token, random token, or left as-is
         loss_mask = ~torch.isin(tokenized_sequence, Tensor(self.tokenizer.all_special_ids))
 
@@ -108,7 +108,7 @@ class InMemoryCSVDataset(Dataset):
         df = pd.read_csv(csv_path)
         sequences = df["sequences"].tolist()
 
-        if "label" in df.columns:
+        if "labels" in df.columns:
             labels = df["labels"].tolist()
         else:
             labels = []
